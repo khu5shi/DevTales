a9c2574acd00ced8c988d229d314590fbe6c1cb6
Khushi Agrawal <khushi4608@gmail.com>
2025-11-28 00:42:17 +0530

    added more stuff

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,11 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Blog from "./pages/Blog"
+import Layout from "./pages/admin/Layout"
+import Dashboard from "./pages/admin/Dashboard"
+import Addblog from "./pages/admin/Addblog"
+import Listblog from "./pages/admin/Listblog"
+import Comment from "./pages/admin/Comment"
 
 function App() {
   
@@ -10,6 +15,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/blog/:id" element={<Blog/>}/>
+        <Route path="/admin" element={<Layout/>}>
+        <Route index element={<Dashboard/>}/>
+        <Route path='addBlog' element={<Addblog/>}/>
+         <Route path='listBlog' element={<Listblog/>}/>
+          <Route path='comments' element={<Comment/>}/>
+        </Route>
       </Routes>
     </div>
   )
